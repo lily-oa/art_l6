5faef190515c410c6e428633b791dd0834b02145
lily <a0976198586@gmail.com>
2022-01-01 17:34:19 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,165 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+//動畫
+
+AOS.init();
+$(function(){
+  AOS.init({
+  offset: 200, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 1000, // values from 0 to 3000, with step 50ms
+  once: true,
+  });
+
+  resetForm();
+  checkSubscriptionValue();
+
 });
+
+/*---------------------------------------------- */
+
+
+// loader-inner
+$(function () {
+  $('html, body').animate({
+    scrollTop: 0
+  }, 100);
+  $(".loader-inner").loaders();
+  $('body').css('overflow', 'hidden');
+  setTimeout(function () {
+    $('body').css('overflow', 'auto');
+    $(".loading").fadeOut(600);
+  }, 800);
+});
+
+// sweetalert 設定值
+const loginModal = new bootstrap.Modal($('#loginModal'));
+const registerModal = new bootstrap.Modal($('#registerModal'));
+const forgotPasswordModal = new bootstrap.Modal($('#forgotPasswordModal'));
+
+$('.forgotPasswordModal-btn').on('click', () => {
+  loginModal.hide();
+  forgotPasswordModal.show();
+});
+
+$('.registerModal-btn').on('click', () => {
+  loginModal.hide();
+  registerModal.show();
+});
+
+/*---------------------------------------------- */
+// 重設密碼連結寄出訊息
+$('.searchPassword-check').on('submit', function(event){
+  event.preventDefault();
+  forgotPasswordModal.hide();
+  swal.fire({
+    icon: 'success',
+    title: '重設密碼連結已寄出',
+    text: '模擬訊息',
+  });
+});
+
+//登入成功訊息
+$('.loginModal-check').on('submit', function(event){
+  event.preventDefault();
+  loginModal.hide();
+  swal.fire({
+    icon: 'success',
+    title: '登入成功',
+    text: '模擬訊息', 
+  });
+});
+
+//註冊一個帳號成功訊息
+$('.registerModal-check').on('submit', function(event){
+  event.preventDefault();
+  registerModal.hide();
+  swal.fire({
+    icon: 'success',
+    title: '註冊成功',
+    text: '模擬訊息',
+  });
+});
+
+// 按下"前往登入"按鈕後
+$('.loginModal-btn').on('click', () => {
+  registerModal.hide();
+  loginModal.show();
+});
+
+// detail.html 按下"收藏展覽"按鈕後
+$('.js-collert-btn').on('click', function(){
+  const btnContent = $(this).text();
+  swal.fire({
+    text: '已加入收藏',
+  });
+
+  if(btnContent === '收藏展覽'){
+    console.log(btnContent);
+    $(this).text('展覽已收藏').removeClass('btn-outline-primary').addClass('btn-secondary');
+  } else {
+    console.log(btnContent);
+    $(this).text('收藏展覽').removeClass('btn-secondary').addClass('btn-outline-primary');
+  }
+});
+
+// detail.html 按下"購買票卷"按鈕後
+$('.js-add-cart').on('click', function(){
+  swal.fire({
+    text: '展覽已加入購物車',
+  });
+});
+
+// 每個頁面當按下右下方的 "^" 返迴最上方的按鈕時
+// toUpBtn.ejs
+$('#btnColorChange').on('click', function(){
+  $(this).toggleClass('bg-secondary');
+});
+
+
+/*---------------------------------------------- */
+// banner-index.ejs "購買票卷"按鈕買
+function addTicket(showName = '展覽') {
+  swal.fire({
+    title: showName,
+    inputLabel: '你想購買幾張票卷？',
+    icon: 'question',
+    input: 'range',
+    inputAttributes:{
+      min: 1,
+      max: 10,
+      step: 1,
+    },
+    inputValue: 1,
+  });
+}
+
+/*---------------------------------------------- */
+//首頁輪播
+//設定輪播圖片間距
+  const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+      },
+      breakpoints: {
+        576: {
+          slidesPerView: 1.5,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        992: {
+          slidesPerView: 2.5,
+          spaceBetween: 30,
+        },
+        1200: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+      }
+  });
+
+/*---------------------------------------------- */
+
---
+++ app/assets/js/formValidation.js
@@ -0,0 +1,195 @@
+//表單驗證
+function resetForm() {
+  const allInputs = document.querySelectorAll('input');
+  const allErrorMsg = document.querySelectorAll('.js-validate-msg');
+  const allSubmitBtn = document.querySelectorAll('.js-form-submit-btn');
+  const classStyle = ['border-danger', 'animate__animated', 'animate__headShake'];
+
+  allInputs.forEach((input) => {
+    $(input).val('');
+    $(input).removeClass(classStyle);
+  });
+
+  allErrorMsg.forEach((msg) => {
+    $(msg).text('');
+    $(msg).removeClass('d-block');
+  });
+
+  allSubmitBtn.forEach((btn) => {
+    $(btn).siblings('.disabled-style').removeClass('d-none');
+  });
+}
+
+
+function validationForm() {
+  validationEmail();
+  validationPassword();
+  validationUserName();
+  validationPhone();
+
+  validationCreditCardNumber();
+  validationCreditCardDate();
+  validationCreditCardDate();
+  validationCreditCardPassword();
+}
+
+//信箱驗證
+function validationEmail(dom = '.js-user-email') {
+  const data = {
+    inputs: document.querySelectorAll(dom),
+    rule: /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z]+$/,
+    msg: '格式錯誤',
+  };
+  validationAllInputsFn(data);
+}
+
+
+
+
+function validationAllInputsFn(data) {
+  const { inputs, rule, msg } = data;
+
+  inputs.forEach((input) => {
+    $(input).on('input propertychange', function() {
+      const errorName =  $(input).attr('name');
+      const inputDatas = {
+        input: $(this),
+        inputValue: $(this).val(),
+        rule: rule,
+        errorName: errorName,
+        errorCustomMsg: `${errorName}${msg}`,
+      }
+      checkInputDate(inputDatas);
+    });
+  });
+}
+function validationInputFn(data) {
+  const { inputs, rule, msg } = data;
+
+  $(inputs).on('input propertychange', function() {
+    const errorName =  $(inputs).attr('name');
+    const inputDatas = {
+      input: $(this),
+      inputValue: $(this).val(),
+      rule: rule,
+      errorName: errorName,
+      errorCustomMsg: `${errorName}${msg}`,
+    }
+    checkInputDate(inputDatas);
+  });
+}
+
+function checkInputDate(obj) {
+  const borderStyle = ['border-danger', 'animate__animated', 'animate__headShake'];
+  const { input, inputValue, rule, errorName, errorCustomMsg } = obj;
+  const errorMsg = $(input).next();
+
+  if(!rule.test(inputValue)) {
+    input.addClass(borderStyle);
+    $(errorMsg).addClass('d-block');
+
+    if (inputValue === '') {
+      $(errorMsg).text(`${errorName}為必填`);
+    } else {
+      $(errorMsg).text(errorCustomMsg);
+    }
+  } else {
+    input.removeClass(borderStyle);
+    $(errorMsg).removeClass('d-block');
+  }
+}
+
+
+//----------------------------------------------------  
+
+function checkUserName(obj) {
+  const borderStyle = ['border-danger', 'animate__animated', 'animate__headShake'];
+  const { input, inputValue, rule, symbolRule, errorName } = obj;
+  const errorMsg = $(input).next();
+
+  if (!rule.test(inputValue)) {
+    input.addClass(borderStyle);
+    $(errorMsg).addClass('d-block');
+    if(symbolRule.test(inputValue)) {
+      console.log(symbolRule.test(inputValue));
+      $(errorMsg).text(`${errorName}不得含特殊符號或數字`);
+    } else if(inputValue === '') {
+      $(errorMsg).text(`${errorName}為必填`);
+    } else if (inputValue.length <= 1) {
+      input.addClass(borderStyle);
+      $(errorMsg).addClass('d-block');
+      $(errorMsg).text(`${errorName}須超過兩個字以上`);
+    }
+  } else {
+    input.removeClass(borderStyle);
+    $(errorMsg).removeClass('d-block');
+  }
+}
+
+
+$('.js-form-check').on('change', function() {
+  const formInputs = document.querySelectorAll('.js-form-input');
+  checkFormValue(formInputs);
+});
+
+$('.js-modal-login-check').on('change', function() {
+  const formInputs = document.querySelectorAll('.js-modal-login-input');
+  checkFormValue(formInputs);
+});
+
+
+function checkFormValue(inputs) {
+  const submitBtn = $('.js-form-submit-btn');
+  let inputValueTrue = 0;
+
+  inputs.forEach(function(input) {
+    if (input.value !== '') {
+      inputValueTrue += 1;
+    }
+  });
+
+  if (inputValueTrue === inputs.length) {
+    $(submitBtn).siblings('.disabled-style').addClass('d-none');
+  } else {
+    $(submitBtn).siblings('.disabled-style').removeClass('d-none');
+  }
+};
+
+
+
+
+
+$('.js-subscription-input').on('input propertychange', function() {
+  const borderStyle = ['border-danger', 'border-2', 'animate__animated', 'animate__headShake'];
+  const inputValue = $(this).val();
+  const rule = /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z]+$/;
+  const errorName =  $(this).attr('name');
+  const errorMsg = $('.js-subscription-msg');
+  const errorCustomMsg = `${errorName} 格式錯誤`;
+
+  if (inputValue === '') {
+    $(this).removeClass(borderStyle);
+    $(errorMsg).removeClass('d-block');
+    $('.js-subscription-btn').prop('disabled', true);
+
+  } else if(!rule.test(inputValue)) {
+    $(this).addClass(borderStyle);
+    $(errorMsg).addClass('d-block');
+    $(errorMsg).text(errorCustomMsg);
+    $('.js-subscription-btn').prop('disabled', true);
+
+  } else {
+    $(this).removeClass(borderStyle);
+    $(errorMsg).removeClass('d-block');
+    $('.js-subscription-btn').prop('disabled', false);
+  }
+});
+
+function checkSubscriptionValue() {
+  const errorMsg = $('.js-subscription-msg');
+
+  if($('.js-subscription-input').val() === '') {
+    $('.js-subscription-btn').prop('disabled', true);
+    $(errorMsg).removeClass('d-block');
+  }
+}
