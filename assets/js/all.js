d71a63816507d9e00801a0f060877f868fbd3ac9
lily <a0976198586@gmail.com>
2022-01-09 14:58:35 +0800

    Update 2022-01-09T06:58:33.565Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -93,36 +93,6 @@ function addTicket() {
   });
 }
 /*---------------------------------------------- */
-//首頁輪播
-//設定輪播圖片間距
-
-
-var swiper = new Swiper('.swiper-container', {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  loop: true,
-  autoplay: {
-    delay: 3500,
-    disableOnInteraction: false
-  },
-  breakpoints: {
-    576: {
-      slidesPerView: 1.5
-    },
-    768: {
-      slidesPerView: 2
-    },
-    992: {
-      slidesPerView: 2.5,
-      spaceBetween: 30
-    },
-    1200: {
-      slidesPerView: 3,
-      spaceBetween: 30
-    }
-  }
-});
-/*---------------------------------------------- */
 "use strict";
 
 //表單驗證
