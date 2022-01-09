bfaa707066f27216de776ee43043607cb2b2c929
lily <a0976198586@gmail.com>
2022-01-09 14:58:13 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -95,31 +95,3 @@ function addTicket(showName = '展覽') {
 }
 
 /*---------------------------------------------- */
-//首頁輪播
-//設定輪播圖片間距
-  const swiper = new Swiper('.swiper-container', {
-    slidesPerView: 1,
-      spaceBetween: 30,
-      loop: true,
-      autoplay: {
-        delay: 3500,
-        disableOnInteraction: false,
-      },
-      breakpoints: {
-        576: {
-          slidesPerView: 1.5,
-        },
-        768: {
-          slidesPerView: 2,
-        },
-        992: {
-          slidesPerView: 2.5,
-          spaceBetween: 30,
-        },
-        1200: {
-          slidesPerView: 3,
-          spaceBetween: 30,
-        },
-      }
-  });
-/*---------------------------------------------- */
