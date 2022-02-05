b4b3c0eff349b6b30ec22c56ee745de1da5f6bed
lily <a0976198586@gmail.com>
2022-02-05 17:47:39 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -23,7 +23,7 @@ $(function(){
     signUpModal.hide();
     swalFn('註冊成功');
   });
-
+//送出表單時就到 checkPayment.html 這個網頁去
   $('.js-order-form-check').on('submit', function(event) {
     event.preventDefault();
     location.href = './checkPayment.html';
