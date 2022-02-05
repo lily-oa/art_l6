27f84371ee03ed8ec293766fcf203972d2758cd0
lily <a0976198586@gmail.com>
2022-02-05 21:47:25 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -23,12 +23,17 @@ $(function(){
     signUpModal.hide();
     swalFn('註冊成功');
   });
-  
+
 //送出表單時就到 checkPayment.html 這個網頁去
   $('.js-order-form-check').on('submit', function(event) {
     event.preventDefault();
     location.href = './checkPayment.html';
   });
+//送出表單時就到 checkSuccess.html 這個網頁去
+  $('js-payment-form-check').on('submit', function(event){
+    event.preventDefault();
+    location.href = './checkSuccess.html';
+  });  
 });
 
 /*---------------------------------------------- */
